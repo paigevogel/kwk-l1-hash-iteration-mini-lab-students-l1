94e21b1c9a23a2de7489a64c63444e83405fb89a
paigevogel <paigevogel36@gmail.com>
2018-06-06 20:33:25 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,16 +1,27 @@
-
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+  summer_olympics = {
+    "Sydney" => "2000",
+    "Athens" => "2004",
+    "Beijing" => "2008",
+    "London" => "2012"
+  }
+  puts summer_olympics
 end
+create_olympics_hash
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  create_olympics_hash["Atlana"] = "1996" 
 end
+#add_a_key_value_pair
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+ locations = summer_olympics.keys
+ years = summer_olympics.values
+ summer_olympics.each do |locations, years|
+   puts "The #{years} summer olympics took place in #{locations}"
+ end 
 end
+#iterate_through_hash
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
