3629a3c7315a720a2a821c2d0a68377ade7b3d27
paigevogel <paigevogel36@gmail.com>
2018-06-06 20:09:44 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,8 +1,8 @@
 def create_olympics_hash
   summer_olympics = {
-    "Sydney" => "2000"
-    "Athens" => "2004"
-    "Beijing" => "2008"
+    "Sydney" => "2000",
+    "Athens" => "2004",
+    "Beijing" => "2008",
     "London" => "2012"
   }
 end
